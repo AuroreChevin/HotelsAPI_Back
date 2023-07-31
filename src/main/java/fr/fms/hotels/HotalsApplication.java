2aea675095e35cf3370c7dd8f3648639702917f3
Aurore Chevin <chevinaurore@gmail.com>
2023-07-31 14:04:34 +0200

    init project

---
+++ src/main/java/fr/fms/hotels/HotalsApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.hotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotalsApplication.class, args);
+	}
+
+}
